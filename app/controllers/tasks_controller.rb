5d663b204736da64ed207946e867c6f2d80705de
Louis Cox <louiscox951@gmail.com>
2022-11-15 14:40:55 +0000

    Tasks done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
